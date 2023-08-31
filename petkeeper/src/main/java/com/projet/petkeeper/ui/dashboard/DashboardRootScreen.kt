f31fcd1473d79ca1096e05f1c2880e2bd844149e
A-Grunder <alice.grunder@heig-vd.ch>
2023-08-31 20:36:31 +0200

    Dashboard base

--- petkeeper/src/main/java/com/projet/petkeeper/ui/dashboard/DashboardRootScreen.kt
+++ petkeeper/src/main/java/com/projet/petkeeper/ui/dashboard/DashboardRootScreen.kt
@@ -1,17 +1,54 @@
 package com.projet.petkeeper.ui.dashboard
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.projet.petkeeper.ui.theme.PetkeeperTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardRootScreen(
 
 ){
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Filled.Add, "Add a new advert")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     ){
+        paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "This is the Dashboard", fontSize = 30.sp)
+        }
+    }
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DashboardRootScreenPreview() {
+    PetkeeperTheme {
+        DashboardRootScreen()
     }
 }
