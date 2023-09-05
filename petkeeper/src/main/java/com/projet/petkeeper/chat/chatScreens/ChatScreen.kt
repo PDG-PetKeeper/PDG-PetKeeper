62485a89710a6a6b101628c3e95dda250c2db8db
TegestBogale <tegest.g.bogale@gmail.com>
2023-09-05 19:11:10 +0200

    included msg text field & send button & fetched from fs

--- petkeeper/src/main/java/com/projet/petkeeper/chat/chatScreens/ChatScreen.kt
+++ petkeeper/src/main/java/com/projet/petkeeper/chat/chatScreens/ChatScreen.kt
@@ -1,7 +1,5 @@
 package com.projet.petkeeper.chat.chatScreens
 
-
-import android.content.res.Configuration
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
@@ -20,48 +18,123 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.projet.petkeeper.chat.Message
-import com.projet.petkeeper.chat.MessageData
-import com.projet.petkeeper.chat.UserProfile
-import com.projet.petkeeper.chat.UserProfileData
-import com.projet.petkeeper.ui.theme.PetkeeperTheme
+import coil.compose.rememberImagePainter
+import com.projet.petkeeper.data.ChatMessage
+import com.projet.petkeeper.data.UserModel
+import com.projet.petkeeper.utils.fetchUserData
+import com.projet.petkeeper.utils.sendChatMessage
 
 
 @Composable
-fun ChatScreen(messageList: List<Message>, userProfile: UserProfile) {
+fun ChatScreen(chatViewModel: ChatViewModel, userModel: UserModel) {
+
+    val messageList by rememberUpdatedState(chatViewModel.messages.value ?: emptyList())
+    var newMessageText by remember { mutableStateOf("") }
+
+    //mutable state to hold the image URI
+    //var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
     Column {
         // Top Bar with user profile and back arrow
-        TopBar(userProfile)
+        TopBar(userModel)
 
         // Chat messages
-        Conversation(messageList, userProfile)
+        Conversation(messageList, userModel)
+        // Message input field and send button
+        MessageInputField(
+            messageText = newMessageText,
+            onMessageTextChanged = { newMessageText = it },
+            onSend = {
+                // Send text message
+                userModel.userId?.let { userId ->
+                    sendChatMessage(
+                        userId, newMessageText,
+                        onSuccess = {
+                            // Handle success
+                        },
+                        onFailure = {
+                            // Handle failure,
+                        }
+                    )
+                }
+                newMessageText = ""
+            }
+
+        )
     }
 }
 
+
 @Composable
-fun TopBar(userProfile: UserProfile) {
+fun MessageInputField(
+    messageText: String,
+    onMessageTextChanged: (String) -> Unit,
+    onSend: () -> Unit // Common callback for sending text or images
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.tertiary)
+            .padding(8.dp)
+    ) {
+        // Input field for typing messages
+        BasicTextField(
+            value = messageText,
+            onValueChange = { newText ->
+                onMessageTextChanged(newText)
+            },
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)
+                .background(Color.White, shape = RoundedCornerShape(20.dp))
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // Common send button for both text and images
+        IconButton(
+            onClick = { onSend() }, // Invoke the common send callback
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.secondary)
+                .padding(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send"
+            )
+        }
+    }
+}
+
+
+@Composable
+fun TopBar(userModel: UserModel) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -79,19 +152,20 @@ fun TopBar(userProfile: UserProfile) {
             horizontalArrangement = Arrangement.SpaceBetween, // Align children horizontally to start and end,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            // Back arrow (you can use your own icon)
             Icon(
                 imageVector = Icons.Default.ArrowBack,
                 contentDescription = null,
                 modifier = Modifier
                     .size(24.dp)
-                    .clickable { /* Handle back button click */ }
+                    .clickable {
+                    //TODO Handle back button click
+                    }
             )
             Spacer(modifier = Modifier.weight(1f))
 
             // User name
             Text(
-                text = userProfile.name,
+                text = userModel.firstName + " " + userModel.lastName,
                 color = MaterialTheme.colorScheme.secondaryContainer,
                 style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.padding(start = 8.dp)
@@ -103,28 +177,29 @@ fun TopBar(userProfile: UserProfile) {
             Surface(
                 modifier = Modifier.size(40.dp),
                 shape = CircleShape,
-                color = Color.Transparent, // Transparent background
+                color = Color.Transparent,
                 border = BorderStroke(1.5.dp, MaterialTheme.colorScheme.secondary)
             ) {
-                UserProfileData.getUserProfileByName("User1")
-                    ?.let { painterResource(id = it.profileImage) }?.let {
-                        Image(
-                            painter = it,
-                            contentDescription = "User Profile",
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
+                val profileImageUrl = userModel.profileImageUrl // Get the profile image URL
+                val painter = rememberImagePainter(data = profileImageUrl)
+                Image(
+                    painter = painter,
+                    contentDescription = "User Profile",
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
 }
 
+
 @Composable
-fun MessageBox(msg: Message, userProfile: UserProfile) {
+fun MessageBox(msg: ChatMessage, userModel: UserModel) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
+        val profileImageUrl = userModel.profileImageUrl // Get the profile image URL
+        val painter = rememberImagePainter(data = profileImageUrl)
         Image(
-            //TODO change picture to user profile picture
-            painter = painterResource(userProfile.profileImage),
+            painter = painter,
             contentDescription = "Profile picture",
             modifier = Modifier
                 .size(40.dp)
@@ -140,7 +215,7 @@ fun MessageBox(msg: Message, userProfile: UserProfile) {
         )
         Column(modifier = Modifier.clickable { msgExpand = !msgExpand }) {
             Text(
-                msg.sender,
+                text = msg.message,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleMedium
             )
@@ -156,54 +231,77 @@ fun MessageBox(msg: Message, userProfile: UserProfile) {
                     .padding(1.dp)
             ) {
                 Text(
-                    msg.body,
+                    text = msg.message, // Check if msg.text is a String
                     color = MaterialTheme.colorScheme.primary,
                     modifier = Modifier.padding(all = 4.dp),
                     style = MaterialTheme.typography.bodyMedium,
                     maxLines = if (msgExpand) Int.MAX_VALUE else 1
                 )
             }
-
         }
+    }
+}
 
 
+@Composable
+fun Conversation(
+    messageListState: List<ChatMessage>,
+    userModel: UserModel
+) {
+    val messageList = messageListState.reversed()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        messageList.forEach { msg ->
+            MessageBox(msg, userModel)
+        }
+    }
+}
+
+
+@Composable
+fun UserChatScreen(userId: String) {
+    var userModel by remember { mutableStateOf<UserModel?>(null) }
+
+    // Fetch user data when the Composable is first displayed
+    LaunchedEffect(userId) {
+        fetchUserData(userId) { fetchedUserModel ->
+            // Update the userModel state with the fetched data
+            userModel = fetchedUserModel
+        }
     }
 
+    // Check if userModel is null or has data and display accordingly
+    userModel?.let { user ->
+        // Call the ChatScreen Composable with the fetched userModel
+        ChatScreen(chatViewModel = ChatViewModel(), userModel = user)
+    }
 }
 
 @Composable
-fun Conversation(messageList: List<Message>, userProfile: UserProfile) {
-    LazyColumn {
-        items(messageList) { message ->
-            MessageBox(message, userProfile)
+fun MessageInputFieldPreview() {
+    // MessageText variable with some predefined text for the preview
+    var messageText by remember { mutableStateOf("Tired") }
+
+    // Create a preview of the MessageInputField with the messageText variable
+    MessageInputField(
+        messageText = messageText,
+        onMessageTextChanged = { messageText = it },
+        onSend = {
+            // TODO Handle send button click
         }
-    }
+
+    )
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+
 @Composable
-fun PreviewMessageBox() {
-    PetkeeperTheme {
-        UserProfileData.getUserProfileByName("User1")?.let {
-            ChatScreen(
-                messageList = MessageData.MessageList,
-                userProfile = it
-            )
-        }
-
-    }
+@Preview
+fun PreviewMessageInputField() {
+    MessageInputFieldPreview()
 }
 
-@Preview(name = "Light Mode", showBackground = true)
-@Composable
-fun PreviewLightMessageBox() {
-    PetkeeperTheme {
-        UserProfileData.getUserProfileByName("User1")?.let {
-            ChatScreen(
-                messageList = MessageData.MessageList,
-                userProfile = it
-            )
-        }
 
-    }
-}
