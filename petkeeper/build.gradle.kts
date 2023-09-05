7630a4763df348c756a4a316c3af94a25828c888
TegestBogale <tegest.g.bogale@gmail.com>
2023-09-05 19:07:34 +0200

    dependecy issues & deleted files

--- petkeeper/build.gradle.kts
+++ petkeeper/build.gradle.kts
@@ -51,11 +51,11 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.compose.material:material:1.2.0")
+    implementation ("androidx.compose.material:material:1.5.0")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
-    implementation ("com.google.firebase:firebase-auth-ktx:21.1.0")
-    implementation ("com.google.android.gms:play-services-auth:20.4.1")
+    implementation ("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -64,11 +64,16 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.datastore:datastore-core-android:1.1.0-alpha04")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
@@ -89,4 +94,11 @@ dependencies {
     implementation ("com.google.firebase:firebase-analytics-ktx")
     implementation ("com.google.firebase:firebase-auth-ktx")
     implementation ("com.google.firebase:firebase-firestore-ktx")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-alpha01")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.5.0")
+    implementation ("io.coil-kt:coil-compose:1.4.0")
+
+
+
 }
