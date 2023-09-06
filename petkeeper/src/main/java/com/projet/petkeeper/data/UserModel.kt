5369025b36f26c6bf13bf1e224205faa1ef6faa3
TegestBogale <tegest.g.bogale@gmail.com>
2023-09-06 14:44:12 +0200

    used userModel instead of userData

--- petkeeper/src/main/java/com/projet/petkeeper/data/UserModel.kt
+++ petkeeper/src/main/java/com/projet/petkeeper/data/UserModel.kt
@@ -3,47 +3,37 @@ package com.projet.petkeeper.data
 
 data class  UserModel (
     val userId: String? = null,
-    val firstName: String? = null,
-    val lastName: String? = null,
+    val userName: String? = null,
+    //val lastName: String? = null,
     val profileImageUrl: String? = null,
-    val email: String? = null,
+   // val email: String? = null,
 )
 
 
 val userSamples = listOf(
     UserModel(
         userId = "1",
-        firstName = "John",
-        lastName = "Doe",
+        userName = "John",
         profileImageUrl = " ",
-        email = "john@example.com"
     ),
     UserModel(
         userId = "2",
-        firstName = "Jane",
-        lastName = "Doe",
+        userName = "Jane",
         profileImageUrl = " ",
-        email = "jane@example.com"
     ),
     UserModel(
         userId = "3",
-        firstName = "Bob",
-        lastName = "Smith",
+        userName = "Bob",
         profileImageUrl = " ",
-        email = "Bob@example.com"
     ),
     UserModel(
         userId = "4",
-        firstName = "Alice",
-        lastName = "Smith",
+        userName = "Alice",
         profileImageUrl = " ",
-        email = "Alice@example.com"
     ),
     UserModel(
         userId = "5",
-        firstName = "Tom",
-        lastName = "Jones",
+        userName = "Tom",
         profileImageUrl = " ",
-        email = "tom@example.com"
     ),
 )
