3f4302a9c4149b253afaf5b4fea3a2cf7c32d49e
TegestBogale <tegest.g.bogale@gmail.com>
2023-09-06 21:49:32 +0200

    dep for firestore cloud

--- petkeeper/build.gradle.kts
+++ petkeeper/build.gradle.kts
@@ -109,5 +109,8 @@ dependencies {
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-firestore-ktx")
 
+    // FirebaseUI for Cloud Firestore
+    implementation("com.firebaseui:firebase-ui-firestore:7.2.0")
+
 
 }
