3bc48ac352b04aea4d8d46d920bb4387c285d9ff
TegestBogale <tegest.g.bogale@gmail.com>
2023-09-06 12:53:59 +0200

    chat screen linked to chat view

--- petkeeper/src/main/java/com/projet/petkeeper/chat/chatScreens/chatPage/ChatScreen.kt
+++ petkeeper/src/main/java/com/projet/petkeeper/chat/chatScreens/chatPage/ChatScreen.kt
@@ -1,18 +1,35 @@
-package com.projet.petkeeper.chat.chatScreens
+package com.projet.petkeeper.chat.chatScreens.chatPage
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.projet.petkeeper.chat.chatScreens.chatPage.ChatPageViewModel
-import com.projet.petkeeper.chat.chatScreens.chatPage.ChatView
-
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.projet.petkeeper.utils.Appbar
+import com.projet.petkeeper.utils.rememberUserModel
 
 
 @Composable
 fun ChatScreen() {
-    val chatPageViewModel: ChatPageViewModel = viewModel() // Initialize your ViewModel if needed
-    val currentUserModel = // Fetch or obtain the user information as UserModel
+    val chatPageViewModel: ChatPageViewModel = viewModel()
 
-        // Pass the user information to the ChatView
-        ChatView(chatPageViewModel = chatPageViewModel, userModel = currentUserModel)
+    // Fetch the user information (userModel) from Firebase Authentication
+    val currentUser = Firebase.auth.currentUser
+    val currentUserId = currentUser?.uid ?: ""
+    val userModel = rememberUserModel(currentUserId)
+
+
+    if (userModel != null) {
+        // Create the Appbar with the user information
+        Appbar(userModel = userModel!!, action = {
+            // TODO: Add navigation
+        })
+
+        // Pass the user information to the ChatPageView
+        ChatView(chatPageViewModel = chatPageViewModel)
+    } else {
+        // Handle the case where user information is not available
+        // display a loading indicator or an error message
+    }
 }
+
 
